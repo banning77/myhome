dd1bcf9ee8d1d9826cce92a8bd8be6089c19deb5
banning <banning7@gmail.com>
2022-02-19 21:40:35 +0900

    init

---
+++ src/test/java/com/banning/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banning.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
